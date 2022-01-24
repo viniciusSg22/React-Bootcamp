c1b036e2700edc3d3626e662c05906670e2cb95f
Vinícius Sanches Geniselli <vsgeniselli@gmail.com>
2022-01-24 14:43:15 -0300

    Adicionando front(create-react-app) e back end(spring initializr) ao repositório git

---
+++ server/src/main/java/com/vsTech/vsVendas/VsVendasApplication.java
@@ -0,0 +1,13 @@
+package com.vsTech.vsVendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VsVendasApplication.class, args);
+	}
+
+}
