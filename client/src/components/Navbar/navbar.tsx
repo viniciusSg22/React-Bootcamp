ccb552b88d26796bf4d554c233e766a222fd6c5b
Vinícius Sanches Geniselli <vsgeniselli@gmail.com>
2022-01-24 17:20:09 -0300

    Pequenas alterações no Header do aplicativo

--- client/src/components/Navbar/navbar.tsx
+++ client/src/components/Navbar/navbar.tsx
@@ -5,8 +5,8 @@ function Navbar() {
     return (
         <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
             <div className="container">
-                <nav className="my-2 my-md-0 mr-md-3">
-                    <img src={img} alt="vsVendas" /> VSVendas
+                <nav className="my-2 my-md-0 mr-md-3 d-flex align-items-center">
+                    <img src={img} alt="vsVendas" width="40px"/> <strong className="text-primary display-5">VSVendas</strong>
                 </nav>
             </div>
         </div>
