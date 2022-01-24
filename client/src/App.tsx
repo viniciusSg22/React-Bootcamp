265bdd1aa59807710b3ab1be4ab82e18875faa47
Vinícius Sanches Geniselli <vsgeniselli@gmail.com>
2022-01-24 16:40:09 -0300

    Utilizando dependência de gráficos apexcharts para dados mockados

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,5 @@
+import BarChart from 'components/BarChart/barChart';
+import DonutChart from 'components/DonutChart/donutChart';
 import Footer from 'components/Footer/footer';
 import Navbar from 'components/Navbar/navbar';
 import Table from 'components/Table/table';
@@ -8,7 +10,20 @@ function App() {
     <div className="App">
       <Navbar />
       <div className='container'>
-        <h1>Hello World!</h1>
+        <h1 className='py-3'>Dashboarde de vendas!</h1>
+        <div className='row px-3'>
+          <div className='col-sm-6 text-center'>
+            <h5>Porcentagem de sucesso (%)</h5>
+            <BarChart />
+          </div>
+          <div className='col-sm-6 text-center'>
+            <h5>Todas vendas</h5>
+            <DonutChart />
+          </div>
+        </div>
+        <div className='py-3'>
+          <h2>Todas as vendas</h2>
+        </div>
         <Table />
       </div>
       <Footer />
