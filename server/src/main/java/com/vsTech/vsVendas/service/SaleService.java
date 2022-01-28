3348a691d824d57b2467ce74f5ce309c97bba096
Vinícius Sanches Geniselli <vsgeniselli@gmail.com>
2022-01-28 15:45:39 -0300

    Adicionando sistema de busca com páginas contendo um número específico de elementos

---
+++ server/src/main/java/com/vsTech/vsVendas/service/SaleService.java
@@ -0,0 +1,30 @@
+package com.vsTech.vsVendas.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.vsTech.vsVendas.dto.SaleDTO;
+import com.vsTech.vsVendas.entities.Sale;
+import com.vsTech.vsVendas.repositories.SaleRepository;
+import com.vsTech.vsVendas.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+	
+	@Autowired
+	private SaleRepository repository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable) {
+		sellerRepository.findAll();
+		Page<Sale> result = repository.findAll(pageable);
+		return result.map(x -> new SaleDTO(x));
+	}
+}
