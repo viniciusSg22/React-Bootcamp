56cdf49fd4e74fa48238fa6c2be5866868a56c45
Vinícius Sanches Geniselli <vsgeniselli@gmail.com>
2022-01-28 16:48:01 -0300

    Implementando busca por grupos

--- server/src/main/java/com/vsTech/vsVendas/service/SaleService.java
+++ server/src/main/java/com/vsTech/vsVendas/service/SaleService.java
@@ -1,5 +1,7 @@
 package com.vsTech.vsVendas.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.vsTech.vsVendas.dto.SaleDTO;
+import com.vsTech.vsVendas.dto.SaleSuccessDTO;
+import com.vsTech.vsVendas.dto.SaleSumDTO;
 import com.vsTech.vsVendas.entities.Sale;
 import com.vsTech.vsVendas.repositories.SaleRepository;
 import com.vsTech.vsVendas.repositories.SellerRepository;
@@ -26,5 +30,13 @@ public class SaleService {
 		sellerRepository.findAll();
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
+	}
+	
+	public List<SaleSumDTO> amountGroupedBySeller() {
+		return repository.amountGroupedBySeller();
+	}
+	
+	public List<SaleSuccessDTO> successGroupedBySeller() {
+		return repository.successGroupedBySeller();
 	}
 }
