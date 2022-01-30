b2ffbb19c853d9ddcc5cd68dadb93b2a71222f6a
Vinícius Sanches Geniselli <vsgeniselli@gmail.com>
2022-01-30 15:46:54 -0300

    Navegação de home para dashboard e dashboard para home

--- client/src/components/Navbar/navbar.tsx
+++ client/src/components/Navbar/navbar.tsx
@@ -1,16 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import img from "../../assets/img/vsv.png";
 
 function Navbar() {
-    return (
-        <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
-            <div className="container">
-                <nav className="my-2 my-md-0 mr-md-3 d-flex align-items-center">
-                    <img src={img} alt="vsVendas" width="40px"/> <strong className="text-primary display-5">VSVendas</strong>
-                </nav>
-            </div>
-        </div>
-    )
+  return (
+    <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
+      <div className="container">
+        <nav className="my-2 my-md-0 mr-md-3 d-flex align-items-center">
+          <Link to="/">
+            <img src={img} alt="vsVendas" width={"250px"}/>
+            {/* <strong className="text-primary display-5">VSVendas</strong> */}
+          </Link>
+        </nav>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
--- client/src/pages/Home/dashboard.tsx
+++ client/src/pages/Home/dashboard.tsx
@@ -1,9 +1,27 @@
+import Footer from "components/Footer/footer";
+import Navbar from "components/Navbar/navbar";
 import React from "react";
+import { Link } from "react-router-dom";
 
-export default function Home(){
-    return (
-        <div>
-            <h1>Home Page</h1>
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <div className="jumbotron">
+          <h1 className="display-4">vsVendas</h1>
+          <p className="lead">
+            Analise o desempenho das suas vendas por diferentes perspectivas
+          </p>
+          <hr />
+          <p>
+            Esta aplicação consiste em exibir um dashboard a partir de dados
+            fornecidos por um back end construído com Spring Boot.
+          </p>
+          <Link className="btn btn-primary btn-lg" to="/dashboard">Acessar o dashboard</Link>
         </div>
-    )
+      </div>
+      <Footer />
+    </>
+  );
 }
