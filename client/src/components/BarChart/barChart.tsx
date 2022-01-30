3cfc41da393c54eedd12eed308ec94de1d1c1a16
Vinícius Sanches Geniselli <vsgeniselli@gmail.com>
2022-01-30 19:46:36 -0300

    Utilizando hooks para integrar gráfico em barra com informações da API

--- client/src/components/BarChart/barChart.tsx
+++ client/src/components/BarChart/barChart.tsx
@@ -1,34 +1,71 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import Chart from "react-apexcharts";
+import { SaleSuccess } from "types/sale";
+import { round } from "utils/format";
+import { BASE_URL } from "utils/requests";
+
+type SeriesData = {
+  name: string;
+  data: number[];
+};
+type ChartData = {
+  labels: {
+    categories: string[];
+  };
+  series: SeriesData[];
+};
 
 const BarChart = () => {
+  const [chartData, setChartData] = useState<ChartData>({
+    labels: {
+      categories: [],
+    },
+    series: [
+      {
+        name: "",
+        data: [],
+      },
+    ],
+  });
 
-    const options = {
-        plotOptions: {
-            bar: {
-                horizontal: true,
-            }
-        },
-    };
+  useEffect(() => {
+    axios.get(`${BASE_URL}/sales/success-by-seller`).then((res) => {
+      const data = res.data as SaleSuccess[];
+      const myLabels = data.map((x) => x.sellerName);
+      const mySeries = data.map((x) => round((100.0 * x.deals) / x.visited, 1));
 
-    const mockData = {
+      setChartData({
         labels: {
-            categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+          categories: myLabels,
         },
         series: [
-            {
-                name: "% Sucesso",
-                data: [43.6, 67.1, 67.7, 45.6, 71.1]
-            }
-        ]
-    };
-    return (
-        <Chart
-            options={{ ...options, xaxis: mockData.labels }}
-            series={mockData.series}
-            type="bar"
-            height="240"
-        />
-    );
-}
+          {
+            name: "% Sucesso",
+            data: mySeries,
+          },
+        ],
+      });
+      console.log(chartData);
+    });
+  }, []);
+
+  const options = {
+    plotOptions: {
+      bar: {
+        horizontal: true,
+      },
+    },
+  };
+  
+  return (
+    <Chart
+      options={{ ...options, xaxis: chartData.labels }}
+      series={chartData.series}
+      type="bar"
+      height="240"
+    />
+  );
+};
 
 export default BarChart;
